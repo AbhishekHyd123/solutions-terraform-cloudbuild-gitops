9c77f3031852db6f13361120acc7527870d48b93
AbhishekHyd123 <coolabhi.ajsh.akhi@gmail.com>
2020-02-21 15:52:29 +0530

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "extended-signal-268502-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="extended-signal-268502"
